9346c598743f65c552ccc430852c3d1e4be92487
ryanbrashear <ryan.brashear@interapthq.com>
2016-09-30 12:42:06 -0400

    added in ability to add more marker to a map

--- week3Assessment/assessment.js
+++ week3Assessment/assessment.js
@@ -3,19 +3,36 @@ $(function() {
 
     $("button#search").click(function(ev) {
         var zipCode = $("#searchBarText").val();
-        getCoords(zipCode);
+        getCoordsForMap(zipCode);
+        $("#searchBarText").val("");
+    });
+
+
+    $("button#addMarker").click(function(ev) {
+        var zipCode = $("#searchBarText").val();
+        getCoordsForMarker(zipCode);
         $("#searchBarText").val("");
     });
 
 });//onready function
 
-var getCoords = function(zipCode) {
+var getCoordsForMap = function(zipCode) {
     var googleKey = "&key=AIzaSyDJPzeEP8rfaPZRV8gtyT2keKmKR45ROO8";
     $.ajax("https://maps.googleapis.com/maps/api/geocode/json?address=" + zipCode + googleKey).done(function(data) {
         var latitude = data.results[0].geometry.location.lat;
         var longitude = data.results[0].geometry.location.lng; 
         var cityState = data.results[0].formatted_address;
         initMap(latitude, longitude);
+    });
+}
+
+var getCoordsForMarker = function(zipCode) {
+    var googleKey = "&key=AIzaSyDJPzeEP8rfaPZRV8gtyT2keKmKR45ROO8";
+    $.ajax("https://maps.googleapis.com/maps/api/geocode/json?address=" + zipCode + googleKey).done(function(data) {
+        var latitude = data.results[0].geometry.location.lat;
+        var longitude = data.results[0].geometry.location.lng; 
+        var cityState = data.results[0].formatted_address;
+        addNewMarker(latitude, longitude);
     });
 }
 
@@ -37,4 +54,16 @@ function initMap(latitude, longitude) {
 
 
     //Need to make a template and have it dynamically generate maps in this function
+}
+
+function addNewMarker (latitude, longitude) {
+
+    var myLat = Number(latitude);
+    var myLng = Number(longitude);
+
+    var marker = new google.maps.Marker({
+    position: {lat: myLat, lng: myLng},
+    map: map,
+    title: 'Current Location'
+    });
 }
