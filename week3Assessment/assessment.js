dd990e66ab280bef8975a59e89d4e4bbab755b3e
ryanbrashear <ryan.brashear@interapthq.com>
2016-10-02 09:16:29 -0400

    adding final version of maply

--- week3Assessment/assessment.js
+++ week3Assessment/assessment.js
@@ -1,5 +1,6 @@
-$(function() {
+var counter = 0;
 
+$(function() {
 
     $("button#search").click(function(ev) {
         var zipCode = $("#searchBarText").val();
@@ -7,62 +8,33 @@ $(function() {
         $("#searchBarText").val("");
     });
 
-    $("button#addMarker").click(function(ev) {
-        var zipCode = $("#searchBarText").val();
-        getCoordsForMarker(zipCode);
-        $("#searchBarText").val("");
+    $(document).on('click', 'button#remove', function() {
+        var parentDiv = $(this).parent();
+        parentDiv.remove();
     });
+
 
 });//onready function
 
 var getCoordsForMap = function(zipCode) {
     var googleKey = "&key=AIzaSyDJPzeEP8rfaPZRV8gtyT2keKmKR45ROO8";
+    var id = "map" + counter;
     $.ajax("https://maps.googleapis.com/maps/api/geocode/json?address=" + zipCode + googleKey).done(function(data) {
         var latitude = data.results[0].geometry.location.lat;
         var longitude = data.results[0].geometry.location.lng; 
         var cityState = data.results[0].formatted_address;
-        initMap(latitude, longitude);
+
+        var template = $("#template").html();
+
+        template = template.replace("@@cityState@@", cityState);
+        template = template.replace("@@latitude@@", Number(latitude));
+        template = template.replace("@@longitude@@", Number(longitude));
+        template = template.replace("@@ID1@@", id);
+        template = template.replace("@@ID2@@", id);
+
+        $("#maps").append(template);
+        counter++;
+
     });
 }
 
-var getCoordsForMarker = function(zipCode) {
-    var googleKey = "&key=AIzaSyDJPzeEP8rfaPZRV8gtyT2keKmKR45ROO8";
-    $.ajax("https://maps.googleapis.com/maps/api/geocode/json?address=" + zipCode + googleKey).done(function(data) {
-        var latitude = data.results[0].geometry.location.lat;
-        var longitude = data.results[0].geometry.location.lng; 
-        var cityState = data.results[0].formatted_address;
-        addNewMarker(latitude, longitude);
-    });
-}
-
-var map;
-function initMap(latitude, longitude) {
-
-    var myLat = Number(latitude);
-    var myLng = Number(longitude);
-    map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: myLat, lng: myLng},
-    zoom: 8
-    });
-
-    var marker = new google.maps.Marker({
-    position: {lat: myLat, lng: myLng},
-    map: map,
-    title: 'Current Location'
-    });
-
-
-    //Need to make a template and have it dynamically generate maps in this function
-}
-
-function addNewMarker (latitude, longitude) {
-
-    var myLat = Number(latitude);
-    var myLng = Number(longitude);
-
-    var marker = new google.maps.Marker({
-    position: {lat: myLat, lng: myLng},
-    map: map,
-    title: 'Current Location'
-    });
-}
